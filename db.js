dd14d468f370e89d501289caa593d355849427a8
Kim yeonsu <39877181+dustnehowl@users.noreply.github.com>
2022-10-23 22:08:47 +0900

    db connect

---
+++ db.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'y8237922',
+    port:3306,
+    database:'mydb',
+});
+
+function getAllMembers(callback){
+    connection.query(`SELECT * FROM USER`, (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function getLoginUser(user_id, callback){
+    connection.query(`SELECT * FROM USER WHERE email="${user_id}"`, (err, rows, fields) => {
+        if(err){
+            console.log("error!!!");
+            throw err;
+        }
+        callback(rows);
+    });
+}
+
+module.exports = {
+    getAllMembers,
+    getLoginUser
+}
