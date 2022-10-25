c0a95fa07ceee828a886109adc94cbaaaaab8e1c
Kim yeonsu <39877181+dustnehowl@users.noreply.github.com>
2022-10-25 21:29:24 +0900

    ADD playlist db

--- routes/musics.js
+++ routes/musics.js
@@ -13,7 +13,20 @@ router.get('/getAllMusics', (req, res, next) => {
 router.post('/addMusic', (req, res, next) => {
     console.log("새로운 음악을 추가합니다.");
     const new_music = req.body;
-    
+});
+
+router.get('/getMusic', (req, res, next) => {
+    console.log("음악정보를 조회합니다.");
+    const tmp_music = req.body;
+    // json 으로 올라나??
+
+});
+
+router.get('/getAllPlaylist', (req, res, next) => {
+    db.query(`SELECT * FROM PLAYLIST`, (err, rows, fields) => {
+        if(err) throw err;
+        res.send(rows);
+    });
 });
 
 module.exports = router;
