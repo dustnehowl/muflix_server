f38c10901748be6c1482d4b93fecf82f4d360bb3
Kim yeonsu <39877181+dustnehowl@users.noreply.github.com>
2022-11-28 17:20:41 +0900

    Fix add musics_playlist

--- routes/musics.js
+++ routes/musics.js
@@ -57,8 +57,9 @@ router.post('/addPlaylist', (req, res, next) => {
             db.query(`INSERT INTO PLAYLIST
                      ( owner, name, information )
                      VALUE ("${user.id}", "${new_playlist.name}", "${new_playlist.information}");`);
-            db.query(`SELECT id FROM PLAYLIST WHERE owner="${user_id}" AND name="${new_playlist.name}";`, (err, rows, fields) => {
+            db.query(`SELECT * FROM PLAYLIST WHERE owner="${user_id}" AND name="${new_playlist.name}";`, (err, rows, fields) => {
                 if(err) throw err;
+                console.log(rows[0].id);
                 res.send(rows[0]);
             });
             // db.query(`INSERT INTO music_playlist
