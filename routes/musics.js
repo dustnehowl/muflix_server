10452bde1bea3ea41eda2b3b3b89a5c14ce73e23
Kim yeonsu <39877181+dustnehowl@users.noreply.github.com>
2022-12-01 14:47:24 +0900

    Fixing getPlaylist by id API

--- routes/musics.js
+++ routes/musics.js
@@ -48,15 +48,25 @@ router.get('/getAllPlaylist', (req, res, next) => {
 router.get('/getPlaylist/:id', (req, res, next) => {
     console.log("특정 플레이리스트를 조회합니다.");
     var playlistid = req.params.id;
-    db.query(`SELECT * FROM PLAYLIST WHERE id="${playlistid}"`, (err, rows, fields) => {
-        var playlist = rows[0];
-        db.query(`SELECT music_id FROM music_playlist WHERE playlist_id="${playlistid}"`, (err2, rows2, fields2)=>{
-            res.send({
-                "playlist_info" : playlist,
-                "musics" : rows2,
-            });
+    try{
+        db.query(`SELECT * FROM PLAYLIST WHERE id="${playlistid}"`, (err, rows, fields) => {
+            var playlist = rows[0];
+            try{
+                db.query(`SELECT music_id FROM music_playlist WHERE playlist_id="${playlistid}"`, (err2, rows2, fields2)=>{
+                    res.send({
+                        "playlist_info" : playlist,
+                        "musics" : rows2,
+                    });
+                });
+            }
+            catch{
+                res.send("플레이리스트조회 실패;");
+            }
         });
-    });
+    }
+    catch{
+        res.send("없는 플레이 리스트입니다.");
+    }
 });
 
 router.post('/addPlaylist', (req, res, next) => {
