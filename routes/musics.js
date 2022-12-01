c2585efc573e6ae3f238b40298058a5efc05db45
Kim yeonsu <39877181+dustnehowl@users.noreply.github.com>
2022-12-01 14:53:17 +0900

    bequite

--- routes/musics.js
+++ routes/musics.js
@@ -45,19 +45,19 @@ router.get('/getAllPlaylist', (req, res, next) => {
     });
 });
 
-router.get('/getPlaylist/:id', (req, res, next) => {
-    console.log("특정 플레이리스트를 조회합니다.");
-    var playlistid = req.params.id;
-    db.query(`SELECT * FROM PLAYLIST WHERE id="${playlistid}"`, (err, rows, fields) => {
-        var playlist = rows[0];
-        db.query(`SELECT music_id FROM music_playlist WHERE playlist_id="${playlistid}"`, (err2, rows2, fields2)=>{
-            res.send({
-                "playlist_info" : playlist,
-                "musics" : rows2,
-            });
-        });
-    });
-});
+// router.get('/getPlaylist/:id', (req, res, next) => {
+//     console.log("특정 플레이리스트를 조회합니다.");
+//     var playlistid = req.params.id;
+//     db.query(`SELECT * FROM PLAYLIST WHERE id="${playlistid}"`, (err, rows, fields) => {
+//         var playlist = rows[0];
+//         db.query(`SELECT music_id FROM music_playlist WHERE playlist_id="${playlistid}"`, (err2, rows2, fields2)=>{
+//             res.send({
+//                 "playlist_info" : playlist,
+//                 "musics" : rows2,
+//             });
+//         });
+//     });
+// });
 
 router.post('/addPlaylist', (req, res, next) => {
     console.log("새로운 플레이리스트를 추가합니다.");
