d4a420ef1e3e8c8dbf3c65c4a43593914736fdb2
Kim yeonsu <39877181+dustnehowl@users.noreply.github.com>
2022-12-01 14:42:27 +0900

    Add getplaylist by id API

--- routes/musics.js
+++ routes/musics.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db.js');
 var jwt = require('jsonwebtoken');
+const { defaultValueSchemable } = require('sequelize/types/utils.js');
 
 const key = "yeonsu";
 
@@ -41,6 +42,20 @@ router.get('/getAllPlaylist', (req, res, next) => {
     db.query(`SELECT * FROM PLAYLIST`, (err, rows, fields) => {
         if(err) throw err;
         res.send(rows);
+    });
+});
+
+router.get('/getPlaylist/:id', (req, res, next) => {
+    console.log("특정 플레이리스트를 조회합니다.");
+    var playlistid = req.params.id;
+    db.query(`SELECT * FROM PLAYLIST WHERE id="${playlistid}"`, (err, rows, fields) => {
+        var playlist = rows[0];
+        db.query(`SELECT music_id FROM music_playlist WHERE playlist_id="${playlistid}"`, (err2, rows2, fields2)=>{
+            res.send({
+                "playlist_info" : playlist,
+                "musics" : rows2,
+            });
+        });
     });
 });
 
