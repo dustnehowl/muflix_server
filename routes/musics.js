8fd0090b091686dc419aa45f21bf4422a347950e
Kim yeonsu <39877181+dustnehowl@users.noreply.github.com>
2022-12-07 21:20:48 +0900

    Fix bug

--- routes/musics.js
+++ routes/musics.js
@@ -43,14 +43,14 @@ router.delete('/delPlaylist/:id', (req, res, next) =>{
     try{
         var playlistid = req.params.id;
         console.log(playlistid)
-        // db.query(`DELETE FROM PLAYLIST WHERE id="${playlistid}" AND owner="${user_id}";`, (err, rows, fields) => {
-        //     if(err) return res.status(404).json({
-        //         code: 412,
-        //         message: '플레이리스트 소유자가 아닙니다.',
-        //     });
-        //     db.query(`DELETE FROM music_playlist WHERE playlist_id="${playlistid}";`);
-        //     res.send("플레이리스트 삭제 완료");
-        // });
+        db.query(`DELETE FROM PLAYLIST WHERE id="${playlistid}" AND owner="${user_id}";`, (err, rows, fields) => {
+            if(err) return res.status(404).json({
+                code: 412,
+                message: '플레이리스트 소유자가 아닙니다.',
+            });
+            db.query(`DELETE FROM music_playlist WHERE playlist_id="${playlistid}";`);
+            res.send("플레이리스트 삭제 완료");
+        });
     }
     catch (e){
         res.send(e);
